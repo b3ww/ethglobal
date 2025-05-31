c0394e76216f3e6e8c66fd629f4b79e33d43d495
David <david.gozlan@epitech.eu>
2025-05-31 21:34:53 +0200

    feat: template

--- back/src/github/templates.rs
+++ back/src/github/templates.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
+use alloy::primitives::U256;
+use chrono::{DateTime, Duration, TimeZone, Utc};
 use tera::{Context, Tera};
+use tracing_subscriber::fmt::time::SystemTime;
 
 fn generate_template(values: HashMap<String, String>, name: &str) -> String {
     let tera = Tera::new("templates/**/*.tera").expect("Erreur lors du chargement des templates");
@@ -9,6 +12,23 @@ fn generate_template(values: HashMap<String, String>, name: &str) -> String {
     });
 
     tera.render(name, &context).unwrap()
+}
+
+fn format_type<T>(elem: T) -> String
+where
+    T:std::fmt::Display
+{
+    elem.to_string()
+}
+
+pub fn u256_to_utc_string(timestamp: U256) -> String {
+    let seconds = timestamp.to::<u64>();
+    
+    // Convert to DateTime<Utc>
+    let datetime: DateTime<Utc> = Utc.timestamp_opt(seconds as i64, 0).unwrap();
+    
+    // Format as string (customize format as needed)
+    datetime.format("%Y-%m-%d %H:%M:%S UTC").to_string()
 }
 
 pub fn close_issue() -> String {
@@ -38,16 +58,16 @@ pub fn new_pull_request() -> String {
 }
 
 
-pub fn open_issue() -> String {
+pub fn open_issue(amount: U256, deadline: U256) -> String {
     let tera = Tera::new("templates/**/*.tera").expect("Failed to load templates");
 
     let mut context = Context::new();
     context.insert("grant_creator_address", "0xABC123...DEF");
-    context.insert("grant_amount", "100.00");
+    context.insert("grant_amount", &format_type(amount));
     context.insert("contract_address", "0xCONTRACT123...XYZ");
     context.insert("gas_fee", "0.1");
     context.insert("tx_hash", "0x12345...XYZ");
-    context.insert("deadline", "15 january");
+    context.insert("deadline", &u256_to_utc_string(deadline));
 
     tera.render("open_issue.tera", &context).unwrap()
 }
