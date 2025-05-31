058d218c2a72ed0b19eafa226f98e57b5f83fbff
David <david.gozlan@epitech.eu>
2025-05-31 21:12:39 +0200

    fix: template + added deadline

--- back/src/github/templates.rs
+++ back/src/github/templates.rs
@@ -11,14 +11,6 @@ fn generate_template(values: HashMap<String, String>, name: &str) -> String {
     tera.render(name, &context).unwrap()
 }
 
-// fn m() -> String {
-//     generate_template(HashMap::from([
-//         ("grant_creator_address".to_string(), "0xABC123...DEF".to_string()),
-//         ("grant_amount".to_string(), "100.00".to_string()),
-//         ("contract_address".to_string(), "0xCONTRACT123...XYZ".to_string()),
-//     ]), "open_issue.tera")
-// }
-
 pub fn close_issue() -> String {
     let tera = Tera::new("templates/**/*.tera").expect("Failed to load templates");
 
@@ -46,26 +38,43 @@ pub fn new_pull_request() -> String {
 }
 
 
-pub fn open_issue() {
+pub fn open_issue() -> String {
     let tera = Tera::new("templates/**/*.tera").expect("Failed to load templates");
 
     let mut context = Context::new();
     context.insert("grant_creator_address", "0xABC123...DEF");
     context.insert("grant_amount", "100.00");
     context.insert("contract_address", "0xCONTRACT123...XYZ");
+    context.insert("gas_fee", "0.1");
+    context.insert("tx_hash", "0x12345...XYZ");
+    context.insert("deadline", "15 january");
 
     tera.render("open_issue.tera", &context).unwrap()
 }
 
+pub fn increase_deadline() -> String {
+    let tera = Tera::new("templates/**/*.tera").expect("Failed to load templates");
+
+    let mut context = Context::new();
+    context.insert("gas_fee", "0.1");
+    context.insert("tx_hash", "0x12345...XYZ");
+    context.insert("deadline", "15 january");
+    context.insert("new_deadline", "20 january");
+    context.insert("contract_address", "0xCONTRACT123...XYZ");
+
+    tera.render("increase_deadline.tera", &context).unwrap()
+}
+
+
 mod test {
-    use crate::github::Bot;
+    use crate::github::{templates::{increase_deadline, open_issue}, Bot};
 
     use super::close_issue;
 
     #[tokio::test]
     async fn toto() {
         let bot = Bot::try_new("").unwrap();
-        let _ = bot.add_issue_comment("b3ww", "vGrant", 9, &close_issue()).await;
+        let _ = bot.add_issue_comment("b3ww", "vGrant", 9, &increase_deadline()).await;
         ()
     }
 }
